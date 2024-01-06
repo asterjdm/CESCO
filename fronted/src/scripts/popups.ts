c6b2087600d0c3dcc9e1827b1362375c3adcfbe9
JdM <julien@rmbi.ch>
2024-01-06 22:08:38 +0100

    added popups

---
+++ fronted/src/scripts/popups.ts
@@ -0,0 +1,41 @@
+
+
+export async function openPopup(popupId: string)
+{
+    document.body.style.overflow = "hidden";
+    const popup = document.getElementById(popupId);
+
+    if (popup == null)
+    {
+        throw "Popup ID not found"   
+    }
+
+    popup.classList.add("show");
+}
+
+export async function closePopup(popupId: string) {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+    if (hamburgerMenu == null)
+    {
+        throw ".hamburger-menu not found"   
+    }
+
+    if (hamburgerMenu.classList.contains("active")) 
+    {
+        hamburgerMenu.classList.toggle("active")
+    }
+
+    document.body.style.overflow = "auto";
+
+    const popup = document.getElementById(popupId);
+
+    if (popup == null)
+    {
+        throw "Popup ID not found"   
+    }
+    
+    setTimeout(() => {
+        popup.classList.remove('show', 'closing');
+    }, 250);
+}
