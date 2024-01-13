51fa04afac48cbdff0f7975397d47845b657ac2e
JdM <julien@rmbi.ch>
2024-01-13 14:32:35 +0100

    return formated post with html element object instead of string

--- fronted/src/scripts/getPosts.ts
+++ fronted/src/scripts/getPosts.ts
@@ -9,32 +9,135 @@ export async function getPosts(max: number): Promise<[Post]>
     return posts;
 }
 
-export function formatePost(postData: Post): string
-{
-    let postHtml = /*html*/ `
-        <div id="post_${postData.ID}" class="supra-post">
-            <div class="post-message">
-                <div class="post-message-div">
-                    <img style='cursor: pointer;' class="post-message-div-picture" src="${postData.author_profile_picture}" width="55" height="55" alt="${postData.author} profile picture" onclick="openUserPagePopup(${postData.USER_FK})">
-                    <div class="post-message-div-infos">
-                        <p>${postData.author}</p>
-                        <p>${postData.date}</p>
-                    </div>
-                </div>
 
-                <div class="post-message-text">
-                    ${postData.content}
-                </div>
-                <footer class="post-message-footer">
-                    <button class="post-message-footer-button except" onclick="openCommentsPopup(${postData.ID})" ><img class="post-message-footer-button-img" src="./public/comments.svg" alt="comment button"></button>
-                    <button class="post-message-footer-button" onclick="voteAndUpdatePost(${postData.ID}, 2)" ><img class="post-message-footer-button-img" src="./public/expandWhite.svg" alt="comment button"><p class="post-message-footer-button-text">${postData.votes_positives_count}</p></button>
-                    <button class="post-message-footer-button" onclick="voteAndUpdatePost(${postData.ID}, 1)" ><img class="post-message-footer-button-img" src="./public/middlevote.svg" alt="comment button"><p class="post-message-footer-button-text">${postData.votes_neutrals_count}</p></button>
-                    <button class="post-message-footer-button" onclick="voteAndUpdatePost(${postData.ID}, 0)" ><img class="post-message-footer-button-img" src="./public/downvote.svg" alt="comment button"><p class="post-message-footer-button-text">${postData.votes_negatives_count}</p></button>
-                    <button class="post-message-footer-button except" onclick="report(${postData.ID})"><img class="post-message-footer-button-img" src="./public/report.svg" alt="comment button"></button>
-                </footer>
-            </div>
-        </div>
-    `
+export function formatePost(postData: Post): HTMLDivElement {
+    let postDiv = document.createElement('div');
+    postDiv.id = `post_${postData.ID}`;
+    postDiv.className = 'supra-post';
 
-    return postHtml;
+    let globalPostMessageDiv = document.createElement('div');
+    globalPostMessageDiv.className = 'post-message';
+
+    let postMessageDiv = document.createElement('div');
+    postMessageDiv.className = 'post-message-div';
+
+    let postMessageDivPicture = document.createElement('img');
+    postMessageDivPicture.style.cursor = 'pointer';
+    postMessageDivPicture.className = 'post-message-div-picture';
+    postMessageDivPicture.src = postData.author_profile_picture;
+    postMessageDivPicture.width = 55;
+    postMessageDivPicture.height = 55;
+    postMessageDivPicture.alt = `${postData.author} profile picture`;
+    postMessageDivPicture.onclick// = () => openUserPagePopup(postData.USER_FK);
+
+    let postMessageDivInfos = document.createElement('div');
+    postMessageDivInfos.className = 'post-message-div-infos';
+
+    let authorParagraph = document.createElement('p');
+    authorParagraph.textContent = postData.author;
+
+    let dateParagraph = document.createElement('p');
+    dateParagraph.textContent = postData.date;
+
+    postMessageDivInfos.appendChild(authorParagraph);
+    postMessageDivInfos.appendChild(dateParagraph);
+
+    let postMessageDivContent = document.createElement('div');
+    postMessageDivContent.className = 'post-message-text';
+    postMessageDivContent.innerHTML = postData.content;
+
+    let postMessageFooter = document.createElement('footer');
+    postMessageFooter.className = 'post-message-footer';
+
+    let commentsButton = document.createElement('button');
+    commentsButton.className = 'post-message-footer-button except';
+    commentsButton.onclick// = () => openCommentsPopup(postData.ID);
+
+    let commentsButtonImg = document.createElement('img');
+    commentsButtonImg.className = 'post-message-footer-button-img';
+    commentsButtonImg.src = './public/comments.svg';
+    commentsButtonImg.alt = 'comment button';
+
+    commentsButton.appendChild(commentsButtonImg);
+
+    let positiveVotesButton = document.createElement('button');
+    positiveVotesButton.id = `vote_2_button_${postData.ID}`;
+    positiveVotesButton.className = 'post-message-footer-button';
+
+    let positiveVotesButtonImg = document.createElement('img');
+    positiveVotesButtonImg.className = 'post-message-footer-button-img';
+    positiveVotesButtonImg.src = './public/expandWhite.svg';
+    positiveVotesButtonImg.alt = 'comment button';
+
+    let positiveVotesCount = document.createElement('p');
+    positiveVotesCount.id = 'votes_count_2';
+    positiveVotesCount.className = 'post-message-footer-button-text';
+    positiveVotesCount.textContent = postData.votes_positives_count;
+
+    positiveVotesButton.appendChild(positiveVotesButtonImg);
+    positiveVotesButton.appendChild(positiveVotesCount);
+
+    let neutralVotesButton = document.createElement('button');
+    neutralVotesButton.id = `vote_1_button_${postData.ID}`;
+    neutralVotesButton.className = 'post-message-footer-button';
+
+    let neutralVotesButtonImg = document.createElement('img');
+    neutralVotesButtonImg.className = 'post-message-footer-button-img';
+    neutralVotesButtonImg.src = './public/middlevote.svg';
+    neutralVotesButtonImg.alt = 'comment button';
+
+    let neutralVotesCount = document.createElement('p');
+    neutralVotesCount.id = 'votes_count_1';
+    neutralVotesCount.className = 'post-message-footer-button-text';
+    neutralVotesCount.textContent = postData.votes_neutrals_count;
+
+    neutralVotesButton.appendChild(neutralVotesButtonImg);
+    neutralVotesButton.appendChild(neutralVotesCount);
+
+    let negativeVotesButton = document.createElement('button');
+    negativeVotesButton.id = `vote_0_button_${postData.ID}`;
+    negativeVotesButton.className = 'post-message-footer-button';
+
+    let negativeVotesButtonImg = document.createElement('img');
+    negativeVotesButtonImg.className = 'post-message-footer-button-img';
+    negativeVotesButtonImg.src = './public/downvote.svg';
+    negativeVotesButtonImg.alt = 'comment button';
+
+    let negativeVotesCount = document.createElement('p');
+    negativeVotesCount.id = 'votes_count_0';
+    negativeVotesCount.className = 'post-message-footer-button-text';
+    negativeVotesCount.textContent = postData.votes_negatives_count;
+
+    negativeVotesButton.appendChild(negativeVotesButtonImg);
+    negativeVotesButton.appendChild(negativeVotesCount);
+
+    let reportButton = document.createElement('button');
+    reportButton.className = 'post-message-footer-button except';
+    reportButton.onclick// = () => report(postData.ID);
+
+    let reportButtonImg = document.createElement('img');
+    reportButtonImg.className = 'post-message-footer-button-img';
+    reportButtonImg.src = './public/report.svg';
+    reportButtonImg.alt = 'comment button';
+
+
+    reportButton.appendChild(reportButtonImg);
+
+    postMessageFooter.appendChild(commentsButton);
+    postMessageFooter.appendChild(positiveVotesButton);
+    postMessageFooter.appendChild(neutralVotesButton);
+    postMessageFooter.appendChild(negativeVotesButton);
+    postMessageFooter.appendChild(reportButton);
+
+    postMessageDiv.appendChild(postMessageDivPicture);
+    postMessageDiv.appendChild(postMessageDivInfos);
+
+    globalPostMessageDiv.appendChild(postMessageDiv);
+    globalPostMessageDiv.appendChild(postMessageDivContent);
+    globalPostMessageDiv.appendChild(postMessageFooter);
+
+    postDiv.appendChild(globalPostMessageDiv);
+
+    return postDiv;
 }
+
