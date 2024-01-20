cc6fa0cc64b746b1781fbf5fe6ad59207a6f4a8b
JdM <julien@rmbi.ch>
2024-01-20 16:46:16 +0100

    added "follow" function

---
+++ fronted/src/scripts/follow.ts
@@ -0,0 +1,14 @@
+
+export async function follow(userId: number): Promise<any>{
+    var formData = new FormData();
+
+    formData.append('id', userId.toString());
+
+    let response = await fetch("https://rmbi.ch/cesco/api/follow.php", {
+        method: "POST", 
+        body: formData,
+        credentials: "include"
+    });
+
+    return response
+}
