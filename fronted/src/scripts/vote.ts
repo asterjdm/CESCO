52194d4f0b9d9fa19bff09a5a3f95aff9af5ee2b
JdM <julien@rmbi.ch>
2024-01-20 16:12:41 +0100

    update all vote button counter

--- fronted/src/scripts/vote.ts
+++ fronted/src/scripts/vote.ts
@@ -5,10 +5,13 @@ import { sendVote } from "./sendVote";
 
 export async function vote(postId: number, voteType: number) {
     const responseData = await sendVote(postId, voteType);
-    
-    const voteCounter = document.getElementById(`post_${postId}`)?.querySelector(`#votes_count_${voteType}`);
-    
-    if (voteCounter) {
+    const postList = document.querySelectorAll(`#post_${postId}`);
+
+    postList.forEach((post) => {
+        const voteCounter = post.querySelector(`#votes_count_${voteType}`) as HTMLElement;
         voteCounter.textContent = responseData.votes_count;
-    }
+        
+    });
+    
+
 }
