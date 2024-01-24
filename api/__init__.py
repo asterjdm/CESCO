96ee9acd5e856db99af6613b2f6ee7ceebde636a
JdM <julien@rmbi.ch>
2024-01-24 16:05:14 +0100

    modified:   api/__init__.py

--- api/__init__.py
+++ api/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from src.newPost import insertNewPost
 from src.newUser import insertNewUser
 
@@ -8,16 +8,15 @@ app = Flask(__name__)
 def hello():
     return 'Hello, World!'
 
-@app.route('/newUser', methods=['POST'])
+@app.route('/newUser', methods=[ 'GET'])
 def newUser():
-    data = request.get_json()
-    username = data.get("username")
-    password = data.get("password")
+    username = request.args["username"]
+    password = request.args["password"]
     
     if username and password:
         return insertNewUser(username, password)
     else:
-        return "Invalid input. Provide username and password."
+        return jsonify({"error":"Invalid input. Provide username and password."})
 
 
 if __name__ == '__main__':
