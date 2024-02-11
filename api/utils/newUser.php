103e79d98b0599e1509199a082c124b839622079
JdM <julien@rmbi.ch>
2024-02-11 15:20:30 +0100

    added newUser function utils

---
+++ api/utils/newUser.php
@@ -0,0 +1,53 @@
+<?php
+include_once(dirname(__FILE__) . "/db.php");
+include_once(dirname(__FILE__) . "/passwordSecurity.php");
+
+
+function isUsernameValid($username) {
+    return isset($username) && strlen($username) < 15 && strlen($username) >= 3 && preg_match("/[a-z0-9]/i", $username);
+}
+
+function isPasswordValid($password) {
+    return isset($password) && strlen($password) < 255;
+}
+
+function isUsernameAlreadyTaken($username) {
+    $db = new Database();
+    $safeUsername = $db->escapeStrings($username);
+    $existingUsers = $db->select("SELECT username FROM cesco_users WHERE username = '$safeUsername'");
+
+    return count($existingUsers) >= 1;
+}
+
+function newUser($username, $password) {
+    $db = new Database();
+
+    $safeUsername = $db->escapeStrings($username);
+
+    if (!isUsernameValid($safeUsername)) {
+        return array("error" => "invalid username");
+    } 
+    if (!isPasswordValid($safeUsername)) {
+        return array("error" => "invalid password");
+    }
+    if(isUsernameAlreadyTaken($username)) {
+        return array("error" => "username already taken");
+    }
+
+    $hashedPassword = hashPassword($password);
+
+    $db -> query("INSERT INTO cesco_users (username, passwd, grade, profile_picture) VALUES ('$safeUsername', '$hashedPassword', 'user', 'profileExample.png')");
+
+    $userInfos = $db->select("SELECT * FROM cesco_users WHERE username = '$username' AND passwd = '$hashedPassword'");
+    $userID = $userInfos[0]["ID"];
+    $userGrade = $userInfos[0]["grade"];
+
+    session_start();
+
+    $_SESSION['userID'] = $db->escapeStrings($userID);
+    $_SESSION['userGrade'] = $db->escapeStrings($userGrade);
+    $_SESSION['username'] = $db->escapeStrings(htmlspecialchars($username));
+    return array("success" => true);
+}
+
+?>
