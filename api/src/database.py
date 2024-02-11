373ff8e1edaf189114bce96447e047e0e55a9c5f
JdM <julien@rmbi.ch>
2024-02-11 12:29:17 +0100

    removed python

--- api/src/database.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-
-def getDB():
-    connection = sqlite3.connect("database.db")
-    cursor = connection.cursor()
-
-    return cursor, connection
--- api/src/hashPassword.py
+++
@@ -1,6 +0,0 @@
-import hashlib
-
-def hashPassword(password):
-    hashed_password = hashlib.md5(password.encode("ASCII"))
-
-    return hashed_password.hexdigest()
