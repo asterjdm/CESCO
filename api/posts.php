0a91f4e6e1e25a7e3894d3ced98717d00460ad39
JdM <julien@rmbi.ch>
2024-02-15 18:05:04 +0100

    added posts.php echo

--- api/posts.php
+++ api/posts.php
@@ -4,5 +4,5 @@ header('Content-type: application/json');
 include_once("utils/isConnected.php");
 
 
-
+echo json_encode(getPosts($_GET["id"], $_GET["q"], $_GET["onlyFollowed"], $_GET["userId"], $_GET["sort"]));
 ?>
