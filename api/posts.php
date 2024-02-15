08af5324f8df158a8c88c325f87b0a8b84c6e1fc
JdM <julien@rmbi.ch>
2024-02-15 18:06:28 +0100

    f

--- api/posts.php
+++ api/posts.php
@@ -1,7 +1,11 @@
 <?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 header('Content-type: application/json');
 
-include_once("utils/isConnected.php");
+include_once("utils/getPosts.php");
 
 
 echo json_encode(getPosts($_GET["id"], $_GET["q"], $_GET["onlyFollowed"], $_GET["userId"], $_GET["sort"]));
